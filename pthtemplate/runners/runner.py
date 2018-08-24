c56ddb30749f8cfbadee69ab3a81239ab6b52a80
Tianyao Chen <try.skycn@Gmail.com>
2018-08-24 14:19:03 +0800

    add vanilla runner

--- pthtemplate/runners/runner.py
+++ pthtemplate/runners/runner.py
@@ -1,71 +1,35 @@
 import collections
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.utils.data
 
 from ..utils.misc import tuplize
 
 
 class Runner:
-    def __init__(self,
-                 network,
-                 criterion,
-                 optimizer,
-                 scheduler=None):
-        self._convertings = {}
+    def __init__(self):
+        self._convertings = collections.defaultdict(lambda: tuplize)
         self._updatings = []
         self._loggings = []
 
-        self.network = network
-        self.criterion = criterion
-        self.optimizer = optimizer
-        self.scheduler = scheduler
-
     def converting(self, key, mapping):
+        """
+        Set a converting when "key" is activated.
+        "mapping" should only accept positional arguments.
+        """
         self._convertings[key] = mapping
 
-    def _converting(self, key, *args):
-        return self._convertings.get(key, tuplize)(*tuplize(*args))
-
     def updating(self, condition, updating):
+        """
+        Set an "updating" with "condition".
+        """
         self._updatings.append((condition, updating))
 
     def logging(self, condition, logging):
+        """
+        Set an "logging" with "condition"
+        """
         self._loggings.append((condition, logging))
 
     def _body(self, epoch, iteration, kwargs):
-        self.network.train()
-        parallel = torch.cuda.is_available() and torch.cuda.device_count() > 1
-
-        if parallel:
-            network = nn.DataParallel(self.network)
-        else:
-            network = self.network
-        
-        kwargs.update(network=self.network, criterion=self.criterion)
-
-        source = iteration.data
-        source = self._converting('source', source)
-        kwargs.update(source=source)
-
-        target = source
-        target = self._converting('target', target)
-        kwargs.update(target=target)
-
-        self.optimizer.zero_grad()
-        output = network(*tuplize(source))
-        output = self._converting('output', output)
-        kwargs.update(output=output)
-
-        loss = self.criterion(*tuplize(output), *tuplize(target))
-        loss.backward()
-        kwargs.update(loss=loss)
-
-        torch.nn.utils.clip_grad_norm_(network.parameters(), 1)
-        self.optimizer.step()
-        if self.scheduler and int(iteration) == len(iteration):
-            self.scheduler.step()
+        raise NotImplementedError
 
     def apply(self, epoch, iteration, device=None):
         kwargs = {}
---
+++ pthtemplate/runners/vanilla_runner.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+
+from .runner import Runner
+
+
+class VanillaRunner(Runner):
+    def __init__(self,
+                 network,
+                 criterion,
+                 optimizer,
+                 scheduler=None):
+        super(VanillaRunner, self).__init__()
+        self.network = network
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+
+    def _body(self, epoch, iteration, kwargs):
+        self.network.train()
+        parallel = torch.cuda.is_available() and torch.cuda.device_count() > 1
+
+        if parallel:
+            network = nn.DataParallel(self.network)
+        else:
+            network = self.network
+        
+        kwargs.update(network=self.network, criterion=self.criterion)
+
+        source = iteration.data
+        source = self.convertings['source'](source)
+        kwargs.update(source=source)
+
+        target = source
+        target = self.convertings['target'](target)
+        kwargs.update(target=target)
+
+        self.optimizer.zero_grad()
+        output = network(*tuplize(source))
+        output = self.convertings['output'](output)
+        kwargs.update(output=output)
+
+        loss = self.criterion(*tuplize(output), *tuplize(target))
+        loss.backward()
+        kwargs.update(loss=loss)
+
+        torch.nn.utils.clip_grad_norm_(network.parameters(), 1)
+        self.optimizer.step()
+        if self.scheduler and int(iteration) == len(iteration):
+            self.scheduler.step()
