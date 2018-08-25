21033d70db7b9e177d686e9ae8f0405a08ad7ae9
Tianyao Chen <try.skycn@Gmail.com>
2018-08-25 11:59:44 +0800

    add mkdir_p

--- pthtemplate/utils/misc.py
+++ pthtemplate/utils/misc.py
@@ -6,3 +6,14 @@ def tuplize(*args):
         else:
             expanded.append(x)
     return tuple(x)
+
+def mkdir_p(path):
+    import errno
+    try:
+        os.makedirs(path)
+        print('Created directory {}'.format(path))
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            print('Directory {} already exists.'.format(path))
+        else:
+            raise
